5186cebae5a26e01eb900b906ae7eb4acb807bce
UroojMurtaza786 <107448533+UroojMurtaza786@users.noreply.github.com>
2023-02-20 11:22:59 -0600

    updated the folder

---
+++ VriendApp/app/src/main/java/com/vriend/app/HomeActivity.java
@@ -0,0 +1,50 @@
+package com.vriend.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.vriend.app.databinding.ActivityHomeBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private ActivityHomeBinding binding;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        // Click listener for the Logout Button
+        binding.btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // SignOut the user, finish the Home Screen Activity
+                // and navigate back to the Login Screen.
+                mAuth.signOut();
+                HomeActivity.this.finish();
+                Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in, get the display name of logged in user and show the greeting.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            binding.tvGreeting.setText("Welcome \n" + currentUser.getDisplayName());
+        }
+    }
+}
